7fc43234ec19d93465fcc5cd44238bdf673116de
xuwenyihust <1016635786@qq.com>
2021-03-07 16:15:17 -0500

    Switch to use kafka.

--- flink/src/main/java/kit/pipeline/data/flink/WordCount.java
+++ flink/src/main/java/kit/pipeline/data/flink/WordCount.java
@@ -1,13 +1,18 @@
 package kit.pipeline.data.flink;
 
 import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.common.serialization.SimpleStringSchema;
 import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.windowing.assigners.TumblingProcessingTimeWindows;
 import org.apache.flink.streaming.api.windowing.time.Time;
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer;
 import org.apache.flink.util.Collector;
+
+import java.util.Properties;
 
 
 public class WordCount {
@@ -15,13 +20,15 @@ public class WordCount {
     public static void main(String[] args) throws Exception {
         // Setup flink env
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        // Kafka connection
+        Properties properties = new Properties();
+        properties.setProperty("bootstrap.servers", "kafka:9092");
+        properties.setProperty("group.id", "flink-service");
 
-        String hostname = "127.0.0.1";
-        int port = 9000;
+        DataStream<String> stream = env
+                .addSource(new FlinkKafkaConsumer<>("topic", new SimpleStringSchema(), properties));
 
-        DataStreamSource<String> text = env.socketTextStream(hostname, port);
-
-        SingleOutputStreamOperator<Tuple2<String, Integer>> counts = text.flatMap(new LineSplitter()).keyBy(0).window(TumblingProcessingTimeWindows.of(Time.seconds(5))).sum(1);
+        SingleOutputStreamOperator<Tuple2<String, Integer>> counts = stream.flatMap(new LineSplitter()).keyBy(0).window(TumblingProcessingTimeWindows.of(Time.seconds(5))).sum(1);
         counts.print();
 
         env.execute("Word count");
