3daf94fb4b0291c7c66b54508ca0b7f29af5a46c
xuwenyihust <1016635786@qq.com>
2021-03-07 22:08:32 -0500

    Add behavior schema skeleton.

--- data-generator/src/main/java/kit/pipeline/data/job/DataGenerator.java
+++ data-generator/src/main/java/kit/pipeline/data/job/DataGenerator.java
@@ -1,5 +1,8 @@
 package kit.pipeline.data.job;
 
+import kit.pipeline.data.shared.Behavior;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerRecord;
@@ -9,6 +12,8 @@ import java.util.Properties;
 public class DataGenerator {
 
     public static void main(String[] args) {
+        Logger logger = LoggerFactory.getLogger(DataGenerator.class);
+
         Properties props = new Properties();
         props.put("bootstrap.servers", "kafka:9092");
         props.put("acks", "all");
@@ -19,12 +24,14 @@ public class DataGenerator {
         props.put("value.serializer",
                 "org.apache.kafka.common.serialization.StringSerializer");
 
+        Behavior behaviorTest = new Behavior();
+
         while (true) {
             try (Producer<String, String> producer = new KafkaProducer<String, String>(props)) {
                 producer.send(new ProducerRecord<String, String>("behavior", "key", "wow"));
                 Thread.sleep(1000);
             } catch (InterruptedException e) {
-                System.out.println("Got exception: " + e.getMessage());
+                logger.error("Got exception: {}", e.getMessage());
             }
         }
     }
--- shared/src/main/java/kit/pipeline/data/shared/Behavior.java
+++ shared/src/main/java/kit/pipeline/data/shared/Behavior.java
@@ -1,14 +1,15 @@
 package kit.pipeline.data.shared;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
-@Data
+@AllArgsConstructor
 public class Behavior {
-    int id;
-    int userId;
-    int orderId;
-    int productId;
-    int productCount;
-    String behaviorType;
-    String timestamp;
+    private int id;
+    private int userId;
+    private int orderId;
+    private int productId;
+    private int productCount;
+    private String behaviorType;
+    private String timestamp;
 }
